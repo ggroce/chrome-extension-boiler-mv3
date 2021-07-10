b6ab0618e77cf63aca5a435b179c398158882ddf
ggroce <ggrocedev@gmail.com>
2021-07-10 18:13:16 -0500

    Initial commit with code

---
+++ foreground.js
@@ -0,0 +1,4 @@
+document.querySelector("body > div.L3eUgb > div.o3j99.LLD4me.yr19Zb.LS8OJ > div > img").style.filter = 'drop-shadow(10px 5px 4px black)';
+document.querySelector("body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.FPdoLc.lJ9FBc > center > input.gNO89b").style.filter = 'drop-shadow(2px 2px 10px black)';
+document.querySelector("#gbqfbb").style.filter = 'drop-shadow(2px 2px 10px black)';
+document.querySelector("body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.FPdoLc.lJ9FBc > center > div").style.filter = 'drop-shadow(2px 2px 10px black)';
